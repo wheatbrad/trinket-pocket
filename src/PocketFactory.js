c4a4517dc1059e7e3bf2b157daf8e3b62c54d957
Brad Runnels <runnels.brad@gmail.com>
2020-10-09 20:35:03 -0400

    move methods to prototype

--- src/PocketFactory.js
+++ src/PocketFactory.js
@@ -1,4 +1,31 @@
 
+const store = new WeakMap();
+const pocketPrototype = Object.create(null);
+
+Object.defineProperties(pocketPrototype, {
+    'getPocketedCoins': {
+        value: function () {
+            return store.get(this).coins;
+        }
+    },
+    'getPocketedTrinkets': {
+        value: function () {
+            return store.get(this).trinkets;
+        }
+    },
+    'buyTrinkets': {
+        value: function(numTrinkets = 1) {
+            return; // stub
+        }
+    },
+    'sellTrinkets': {
+        value: function(numTrinkets = 1) {
+            return; // stub
+        }
+    }
+});
+
+
 /**
  * Factory function initializes and returns Pocket objects
  *
@@ -6,18 +33,16 @@
  *
  * @return  {object}    Pocket
  */
-function PocketFactory({coins, trinkets} = {coins:100, trinkets:0}) {
+function PocketFactory({coins, trinkets} = {coins: 100, trinkets: 0}) {
     
-    const obj = Object.create(null);
-    const staticPocket = new WeakMap();
+    const o = Object.create(pocketPrototype);
 
-    obj.buyTrinkets = function (numTrinkets = 1) {} 
+    store.set(o, {
+        coins: (coins | 0) <= 0 ? 0 : coins,
+        trinkets: (trinkets | 0) <= 0 ? 0 : coins
+    });
 
-    obj.sellTrinkets = function (numTrinkets = 1) {}
-
-    obj.coins = coins;
-
-    return obj;
+    return o;
 }
 
 
