5ca082de36ae8c9ae3d7347d5537eba74c312bef
Brad Runnels <runnels.brad@gmail.com>
2020-10-09 22:16:51 -0400

    complete buy and sell methods; corrected trinket ref in

--- src/PocketFactory.js
+++ src/PocketFactory.js
@@ -15,12 +15,23 @@ Object.defineProperties(pocketPrototype, {
     },
     'buyTrinkets': {
         value: function(numTrinkets = 1) {
-            return; // stub
+            const pocket = store.get(this);
+            const cost = numTrinkets * 10;
+
+            if (pocket.coins < cost) return `You only have ${pocket.coins} coins in your pocket.`;
+
+            pocket.coins -= cost;
+            pocket.trinkets += numTrinkets;
         }
     },
     'sellTrinkets': {
         value: function(numTrinkets = 1) {
-            return; // stub
+            const pocket = store.get(this);
+
+            if (pocket.trinkets < numTrinkets) return `You only have ${pocket.trinkets} trinkets available to sell`;
+
+            pocket.trinkets -= numTrinkets;
+            pocket.coins += numTrinkets * 10;
         }
     }
 });
@@ -39,7 +50,7 @@ function PocketFactory({coins, trinkets} = {coins: 100, trinkets: 0}) {
 
     store.set(o, {
         coins: (coins | 0) <= 0 ? 0 : coins,
-        trinkets: (trinkets | 0) <= 0 ? 0 : coins
+        trinkets: (trinkets | 0) <= 0 ? 0 : trinkets
     });
 
     return o;
